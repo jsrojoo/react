2a33bef2ea991bd8fe0ccb9a3d279e9bdde8a4bd
Joseph Rojo <jsimon.rojo@gmail.com>
2017-05-14 00:25:14 +0800

    postbuild

--- src/app/Input.js
+++ src/app/Input.js
@@ -8,27 +8,15 @@ import {connect} from 'react-redux'
 import todoAction from '../actions/todoAction'
 import todosAction from '../actions/todosAction'
 import Todo from "../model/Todo";
+import axios from 'axios';
 
 class TodoInput extends React.Component {
 
     componentWillMount() {
-        var findIP = new Promise(r => {
-            var w = window,
-                a = new (w.RTCPeerConnection || w.mozRTCPeerConnection || w.webkitRTCPeerConnection)({iceServers: []}),
-                b = () => {
-                };
-            a.createDataChannel("");
-            a.createOffer(c => a.setLocalDescription(c, b, b), b);
-            a.onicecandidate = c => {
-                try {
-                    c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)
-                } catch (e) {
-                }
-            }
-        })
-
-        /*Usage example*/
-        findIP.then(ip => console.log(ip)).catch(e => console.error(e))
+        axios.get("http://ipinfo.io/?callback")
+            .then((response) => {
+                this.props.addTodo("Hello " + response.data.ip);
+            });
     }
 
     textChange = (event) => {
